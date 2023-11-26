b3fb21fc5ed171f9c2c2c25f3fff39d77bc6ea61
forumforum <142608712+forumforum@users.noreply.github.com>
2023-11-26 19:45:14 +0900

    itemモデルとテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,34 @@
 class ItemsController < ApplicationController
+  # ログインしていないユーザーはログインページに促す
+  before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item= Item.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
