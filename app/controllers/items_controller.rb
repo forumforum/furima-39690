b3491400db5d8b5ca2da8e62a532221006d49709
forumforum <142608712+forumforum@users.noreply.github.com>
2023-12-08 21:05:34 +0900

    コードレビュー後、修正⓶

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-
   def index
-    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -27,7 +24,4 @@ class ItemsController < ApplicationController
                                  :delivery_time_id, :price).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
 end
