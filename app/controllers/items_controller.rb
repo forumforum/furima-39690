33efb3123840e4302df2d136b71dc04fbaddab0d
forumforum <142608712+forumforum@users.noreply.github.com>
2023-12-10 20:45:47 +0900

    rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only:[:show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
