89e0c5373a2e0dd609c263c7cef236cbc6a1b03b
forumforum <142608712+forumforum@users.noreply.github.com>
2023-12-13 00:34:15 +0900

    itemsコントローラー、メソッド追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_login, only: [:edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -46,6 +47,12 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id #&& @item.order.nil?
+    redirect_to action: :index unless current_user.id == @item.user_id # && @item.order.nil?
+  end
+
+  def move_to_login
+    return if user_signed_in? && current_user.id == @item.user_id
+
+    redirect_to new_user_session_path
   end
 end
