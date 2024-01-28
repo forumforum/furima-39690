ec631f3fc923bfe6e62c0d927f95c97964d05f25
forumforum <142608712+forumforum@users.noreply.github.com>
2024-01-28 21:07:54 +0900

    impotmap修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "item_price", to: "item_price.js"
-pin "card", to: "card.js"pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "card", to: "card.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
