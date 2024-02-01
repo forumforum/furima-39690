c0a3d6449034d01066b20035d3ae710e97ca14c3
forumforum <142608712+forumforum@users.noreply.github.com>
2024-02-01 23:54:18 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
   has_many :comments
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
