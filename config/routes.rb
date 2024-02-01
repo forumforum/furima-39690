5a91189a717d32a17f19f34636f7c3568161653e
forumforum <142608712+forumforum@users.noreply.github.com>
2024-02-01 23:56:38 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create]
     resources :comments, only: :create
+    resources :users do
+      resource :relationships, only: [:create, :destroy]
+    end
   end
 
 end
