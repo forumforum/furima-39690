7918f95d31587804398c54deeb9e234f330e4682
forumforum <142608712+forumforum@users.noreply.github.com>
2024-02-05 15:41:59 +0900

    アクション定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  def create
+    like = current_user.likes.build(comment_id: params[:comment_id])
+    like.save
+    redirect_to root_path
+  end
+
+  def destroy
+    like = Like.find_by(comment_id: params[:comment_id], user_id: current_user.id)
+    like.destroy
+    redirect_to root_path
+  end
 end
