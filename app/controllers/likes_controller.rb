caa869dd5c4812036b8730c1061c1ef1d69eaa57
forumforum <142608712+forumforum@users.noreply.github.com>
2024-02-05 17:47:14 +0900

    非同期処理実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,23 @@
 class LikesController < ApplicationController
+before_action :set_comment
+
   def create
     like = current_user.likes.build(comment_id: params[:comment_id])
     like.save
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(comment_id: params[:comment_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:comment_id])
   end
 end
